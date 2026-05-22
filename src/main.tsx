6a3f4d0f78f13e4afe556a99606c782da63fe36e
Ivan-khodorov <187195904+Ivan-khodorov@users.noreply.github.com>
2026-05-22 23:25:43 +0700

    GitHub Actions

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <ErrorBoundary>
         <ThemeProvider>
-          <BrowserRouter>
+          <BrowserRouter basename="/React-project-setup-Class-components-Error-boundary">
             <App />
           </BrowserRouter>
         </ThemeProvider>
